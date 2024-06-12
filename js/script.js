bf46d3262db2170800620899f3e1dafdde8d85e7
wisdomMichaels <ijarwisdom@gmail.com>
2024-06-12 18:36:22 +0100

    added js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const apiKey = "a5277a670215173962c7d3ae19ba1757";
+
+const weatherData = document.getElementById("weather-data");
+
+const cityData = document.getElementById("city");
+
+const formEl = document.querySelector("form")
+
+formEl.addEventListener{ "submit", (event) => {
+    event.preventDefault();
+
+    const cityData = city
+}
+
+}
